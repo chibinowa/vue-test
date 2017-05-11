f2eebe6bbd44033ed31dceab10f7f115e16aa898
mio <chibinowa@users.noreply.github.com>
2017-05-11 13:09:56 +0900

    fix

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -7,33 +7,6 @@ import member from './modules/member'
 const store = new Vuex.Store({
   modules: {
     member
-  },
-  state: {
-    popupContent: null,
-    menuCurrent: 0,
-    menu: [
-      { id: 1, label: 'かばん', width: 0, left: 0 },
-      { id: 2, label: 'サーバル', width: 0, left: 0 },
-      { id: 3, label: 'フェネック', width: 0, left: 0 },
-      { id: 4, label: 'アライグマ', width: 0, left: 0 }
-    ],
-    x: 50,
-    y: 50,
-    isMove: false
-  },
-  mutations: {
-    setSize(state, arg) {
-      state.menu[arg.id].width = arg.width
-    },
-    setName(state) {
-      state.menu[0].label = 'かばんちゃん'
-    },
-    updateX(state, value) {
-      state.x = value
-    },
-    setMove(state, value) {
-      state.isMove = value
-    }
   }
 })
 
