4e42f276d58d4e9380af69b8345f769975233709
mio3io <2270244+mio3io@users.noreply.github.com>
2017-05-19 23:46:32 +0900

    firebaseのかんたんな認証

--- src/components/Member/PageList.vue
+++ src/components/Member/PageList.vue
@@ -42,7 +42,6 @@ export default {
     })
   },
   beforeDestroy() {
-    console.log('beforeDestroy')
     this.$store.commit('member/destroy')
   },
   components: { MemberModal }
--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,16 +1,61 @@
 <template>
   <div>
     <h1>home</h1>
+    <div class="content">
+      <p>状態：{{ message }}</p>
+      <dl>
+        <dt>ID</dt>
+        <dd>
+          <input type="text" size="20" v-model="email">
+        </dd>
+        <dt>パスワード</dt>
+        <dd>
+          <input type="text" size="20" v-model="password">
+        </dd>
+      </dl>
+      <button @click="login">ログイン</button>
+      <button @click="logout">ログアウト</button>
+    </div>
   </div>
 </template>
 
 <script>
 // import axios from 'axios'
+import firebase from 'firebase'
+import { DemoEMail, DemoPassword } from '@/api/firebase.config'
+
 export default {
-  name: 'home'
+  name: 'home',
+  data() {
+    return {
+      email: DemoEMail,
+      password: DemoPassword,
+      message: ''
+    }
+  },
+  methods: {
+    login() {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+    },
+    logout() {
+      firebase.auth().signOut()
+    }
+  },
+  created() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.message = `${user.email}でログイン中`
+      } else {
+        this.message = `ログインしてない`
+      }
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+.content {
+  margin: 10px auto;
+  max-width: 800px;
+}
 </style>
